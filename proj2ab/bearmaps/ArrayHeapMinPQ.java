f62b81a4f5f5bb8645aaf0974f4319c1675bdcb0
m-contour <m-contour@qq.com>
2023-10-16 19:18:54 +0800

    feat: finish Proj2A

---
+++ proj2ab/bearmaps/ArrayHeapMinPQ.java
@@ -0,0 +1,166 @@
+package bearmaps;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.NoSuchElementException;
+
+public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
+    private final ArrayList<PriorityNode> nodes;
+    private final HashMap<T, Integer> indexes;
+
+    ArrayHeapMinPQ() {
+        nodes = new ArrayList<>();
+        indexes = new HashMap<>();
+    }
+
+    @Override
+    public void add(T item, double priority) {
+        if (contains(item)) {
+            throw new IllegalArgumentException();
+        }
+        nodes.add(new PriorityNode(item, priority));
+        indexes.put(item, size() - 1);
+        ascend(size() - 1);
+    }
+
+    private void ascend(int idx) {
+        int parentIdx = getParentIndex(idx);
+        PriorityNode parent = nodes.get(parentIdx);
+        PriorityNode current = nodes.get(idx);
+        if (parentIdx > 0 && parent.compareTo(current) > 0) {
+            swap(parentIdx, idx);
+            ascend(parentIdx);
+        }
+    }
+
+    private void descend(int idx) {
+        int minimum = idx;
+        int childLeftIdx = getLeftChildIndex(idx);
+        int childRightIdx = getRightChildIndex(idx);
+        if (childLeftIdx <= size() - 1 && nodes.get(minimum).compareTo(nodes.get(childLeftIdx)) > 0) {
+            minimum = childLeftIdx;
+        }
+        if (childRightIdx <= size() - 1 && nodes.get(minimum).compareTo(nodes.get(childRightIdx)) > 0) {
+            minimum = childRightIdx;
+        }
+        if (minimum != idx) {
+            swap(minimum, idx);
+            descend(minimum);
+        }
+    }
+
+    private void swap(int a, int b) {
+        PriorityNode nodeA = nodes.get(a);
+        PriorityNode nodeB = nodes.get(b);
+        nodes.set(b, nodeA);
+        nodes.set(a, nodeB);
+        indexes.put(nodeA.getItem(), b);
+        indexes.put(nodeB.getItem(), a);
+    }
+
+    private int getParentIndex(int k) {
+        return (k + 1) / 2;
+    }
+
+    private int getLeftChildIndex(int k) {
+        return k * 2 + 1;
+    }
+
+    private int getRightChildIndex(int k) {
+        return k * 2 + 2;
+    }
+
+    @Override
+    public boolean contains(T item) {
+        return indexes.get(item) != null;
+    }
+
+    @Override
+    public T getSmallest() {
+        PriorityNode root = nodes.get(0);
+        if (root == null) {
+            throw new NoSuchElementException();
+        }
+        return root.getItem();
+    }
+
+    @Override
+    public T removeSmallest() {
+        PriorityNode root = nodes.get(0);
+        if (root == null) {
+            throw new NoSuchElementException();
+        }
+        swap(0, size() - 1);
+        nodes.remove(size() - 1);
+        indexes.remove(root.getItem());
+        descend(0);
+        return root.getItem();
+    }
+
+    @Override
+    public int size() {
+        return nodes.size();
+    }
+
+    @Override
+    public void changePriority(T item, double priority) {
+        Integer index = indexes.get(item);
+        if (index == null) {
+            throw new NoSuchElementException();
+        }
+        double oldPriority = nodes.get(index).getPriority();
+        nodes.get(index).setPriority(priority);
+        if (oldPriority < priority) {
+            descend(index);
+        } else {
+            ascend(index);
+        }
+    }
+
+    private class PriorityNode implements Comparable<PriorityNode> {
+        private final T item;
+        private double priority;
+
+        PriorityNode(T item, double priority) {
+            this.item = item;
+            this.priority = priority;
+        }
+
+        T getItem() {
+            return item;
+        }
+
+        double getPriority() {
+            return priority;
+        }
+
+        void setPriority(double priority) {
+            this.priority = priority;
+        }
+
+        @Override
+        public int compareTo(PriorityNode other) {
+            if (other == null) {
+                return -1;
+            }
+            return Double.compare(this.getPriority(), other.getPriority());
+        }
+
+        @Override
+        @SuppressWarnings("unchecked")
+        public boolean equals(Object o) {
+            if (o == this) {
+                return true;
+            }
+            if (o == null || o.getClass() != this.getClass() || o.hashCode() != this.hashCode()) {
+                return false;
+            }
+            return ((PriorityNode) o).getItem().equals(getItem());
+        }
+
+        @Override
+        public int hashCode() {
+            return item.hashCode();
+        }
+    }
+}
--- proj2ab/bearmaps/ArrayHeapMinPQTest.java
+++ proj2ab/bearmaps/ArrayHeapMinPQTest.java
@@ -1,7 +1,62 @@
 package bearmaps;
 
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
 
+import static org.junit.Assert.*;
+
+@SuppressWarnings("ALL")
 public class ArrayHeapMinPQTest {
+
+    @Test
+    public void sanityAddTest() {
+        ArrayHeapMinPQ h = new ArrayHeapMinPQ<>();
+        for (int i = 0; i < 10; i++) {
+            h.add(i, i);
+            //make sure put is working via contains
+            assertTrue(h.contains(i));
+        }
+    }
+
+    @Test
+    public void sanitySmallestTest() {
+        ArrayHeapMinPQ h = new ArrayHeapMinPQ<>();
+        for (int i = 0; i < 10; i++) {
+            h.add(i, i);
+            //make sure put is working via contains
+            assertTrue(h.contains(i));
+        }
+        assertEquals(h.removeSmallest(), 0);
+        assertEquals(h.getSmallest(), 1);
+    }
+
+    @Test
+    public void sanityChangePriority() {
+        ArrayHeapMinPQ h = new ArrayHeapMinPQ<>();
+        for (int i = 0; i < 10; i++) {
+            h.add(i, i);
+            //make sure put is working via contains
+            assertTrue(h.contains(i));
+        }
+        h.changePriority(0, 100);
+        assertEquals(h.getSmallest(), 1);
+    }
+
+
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        ArrayHeapMinPQ<Integer> minHeap = new ArrayHeapMinPQ<>();
+        for (int i = 0; i < 200000; i += 1) {
+            minHeap.add(i, 100000 - i);
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("Total time elapsed: " + (end - start) / 1000.0 +  " seconds.");
+
+        long start2 = System.currentTimeMillis();
+        for (int j = 0; j < 200000; j += 1) {
+            minHeap.changePriority(j, j + 1);
+        }
+        long end2 = System.currentTimeMillis();
+        System.out.println("Total time elapsed: " + (end2 - start2) / 1000.0 +  " seconds.");
+//        jh61b.junit.TestRunner.runTests(ArrayHeapMinPQTest.class);
+    }
 }
